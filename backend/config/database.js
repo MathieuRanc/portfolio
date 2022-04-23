110576a0f712e2f70402d3d40bb372b16dd0aebe
Mathieu Ranc <dev@mathieuranc.fr>
2022-04-23 18:30:56 +0200

    database and htaccess

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,3 +9,43 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+
+// const path = require('path');
+// 
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'mysql',
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 3306),
+//         database: env('DATABASE_NAME', 'strapi'),
+//         username: env('DATABASE_USERNAME', 'strapi'),
+//         password: env('DATABASE_PASSWORD', 'strapi'),
+//       },
+//       options: {},
+//     },
+//   },
+// });
