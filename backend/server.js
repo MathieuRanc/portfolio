bb894404c19e3851328250f9c7bcf388e9e62f86
Mathieu Ranc <dev@mathieuranc.fr>
2022-04-24 22:59:03 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
-const strapi = require('strapi')
+const strapi = require('@strapi/strapi')
 
-strapi(/* {...} */).start()
+if (process.env.NODE_ENV == 'development')
+  strapi({ autoReload: { enabled: true } }).start()
+else strapi().start()
